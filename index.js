58159ec41785f2825781e26b1c2d907c4ccf28cb
ansonfwedu <154986524+ansonfwedu@users.noreply.github.com>
2024-06-26 15:59:50 +0800

    (Feature)Created Zoom Webhook reciver, forward to Parse server

--- index.js
+++ index.js
@@ -1,84 +1,154 @@
 import express from "express";
 import * as crypto from "crypto";
 import dotenv from "dotenv";
+import NodeCache from "node-cache";
 
 dotenv.config();
 const app = express();
+const cache = new NodeCache();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const port = 3000;
 
-// Define routes
+const parseServerHeader = {
+  "x-parse-application-id": process.env.PARSE_SERVER_APP_ID,
+  "x-parse-rest-api-key": process.env.PARSE_SERVER_RESTAPI_KEY,
+};
+
+async function parseServerAuth() {
+  const cacheKey = "parse-server-sessionToken";
+  const token = cache.get(cacheKey);
+
+  if (token) {
+    console.log("Session token found in memory cache");
+    return token;
+  }
+
+  const res = await fetch(`${process.env.PARSE_SERVER_URL}/login`, {
+    method: "POST",
+    headers: parseServerHeader,
+    body: JSON.stringify({
+      username: process.env.PARSE_SERVER_USERNAME,
+      password: process.env.PARSE_SERVER_USER_PASSWORD,
+    }),
+  });
+  const authData = await res.json();
+
+  cache.set("parse-server-sessionToken", authData, 3 * 24 * 60 * 60);
+  return authData;
+}
+
+//Test Zoom Webhook , Fung sir account
+app.post("/zoom-webhook/test", async (request, response) => {
+  const body = request.body;
+  console.log(`Request headers:` + JSON.stringify(request.headers, null, 2));
+  console.log(`event: ` + JSON.stringify(body.event, null, 2));
+  console.log(`payload: ` + JSON.stringify(body.payload, null, 2));
+
+  const hashForVaildate = crypto
+    .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
+    .update(body.payload.plainToken)
+    .digest("hex");
+
+  const data = {
+    plainToken: body.payload.plainToken,
+    encryptedToken: hashForVaildate,
+  };
+
+  response.json(data);
+});
+
 app.get("/is_server_running", async (request, response) => {
   const body = request.body;
-  console.log(`Request recive from client: ${body}`);
+  console.log(`Request headers:` + JSON.stringify(request.headers, null, 2));
+  console.log(`event: ` + JSON.stringify(body.event, null, 2));
+  console.log(`payload: ` + JSON.stringify(body.payload, null, 2));
+
   try {
-    const parseServerRepsonse = await fetch(
-      `${process.env.PARSE_SERVER_URL}/functions/test`,
-      {
-        method: "POST",
-      }
-    );
-    const data = await parseServerRepsonse.json();
-    response.send(data);
+    console.log("Login to Parse Server");
+
+    const authData = await parseServerAuth();
+    if (authData.sessionToken) {
+      console.log(`Login to Parse server success`);
+      const parseServerRes = await fetch(
+        `${process.env.PARSE_SERVER_URL}/functions/test`,
+        {
+          method: "POST",
+          headers: {
+            ...parseServerHeader,
+            "x-parse-session-token": authData.sessionToken,
+          },
+        }
+      );
+      const data = await parseServerRes.json();
+      response.send(data);
+    }
   } catch (error) {
     response.send(error);
   }
 });
 
-app.post("/zoomWebhook", async (request, response) => {
+app.post("/zoom-webhook", async (request, response) => {
   const body = request.body;
+  console.log(`Request headers:` + JSON.stringify(request.headers, null, 2));
+  console.log(`event: ` + JSON.stringify(body.event, null, 2));
+  console.log(`payload: ` + JSON.stringify(body.payload, null, 2));
 
-  console.log(`Recive Zoom Webhook data: ${body}`);
-  console.log(`Recive Zoom Webhook header: ${request.headers}`);
+  //Get specify Zoom Webhook token from parse server
+  const authData = await parseServerAuth();
+  if (authData.sessionToken) {
+    console.log("Login to Parse server success");
+    try {
+      const parseServerRes = await fetch(
+        `${process.env.PARSE_SERVER_URL}/functions/getZoomWebhookSecret`,
+        {
+          method: "POST",
+          headers: {
+            ...parseServerHeader,
+            "x-parse-session-token": authData.sessionToken,
+          },
+          body: JSON.stringify({
+            zoom_events: { headers: request.headers, body: body },
+          }),
+        }
+      );
+      const parseServerData = await parseServerRes.json();
 
-  const message = `v0:${
-    request.headers["x-zm-request-timestamp"]
-  }:${JSON.stringify(body)}`;
+      console.log(`Successfully get secret token from Parse server`);
 
-  const hashForVerify = crypto
-    .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
-    .update(message)
-    .digest("hex");
+      if (body.event === "endpoint.url_validation") {
+        const hashForVaildate = crypto
+          .createHmac("sha256", parseServerData["result"])
+          .update(body.payload.plainToken)
+          .digest("hex");
 
-  const signature = `v0=${hashForVerify}`;
+        const data = {
+          plainToken: body.payload.plainToken,
+          encryptedToken: hashForVaildate,
+        };
 
-  if (request.header["x-zm-signature"] === signature) {
-    if (body.event === "endpoint.url_validation") {
-      console.log("Request vaildate");
-      const hashForVaildate = crypto
-        .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
-        .update(body.payload.plainToken)
-        .digest("hex");
+        response.json(data);
+      } else {
+        fetch(`${process.env.PARSE_SERVER_URL}/functions/zoomWebhook`, {
+          method: "POST",
+          headers: {
+            ...parseServerHeader,
+            "x-parse-session-token": authData.sessionToken,
+          },
+          body: JSON.stringify({
+            zoom_events: { headers: request.headers, body: body },
+          }),
+        });
 
-      const data = {
-        plainToken: body.payload.plainToken,
-        encryptedToken: hashForVaildate,
-      };
-
-      response.json(data);
-    }else{
-      try {
-        const parseServerRepsonse = await fetch(
-          `${process.env.PARSE_SERVER_URL}/functions/zoomWebhook`,
-          {
-            method: "POST",
-            body: body
-          }
-        );
-        const data = await parseServerRepsonse.json();
-        console.log(`Parse Server Api response: \n${data}`)
-      } catch (error) {
-        console.log(`Error occurred: ${error}`)
+        response.json({ status: 200, message: "Zoom event forward success" });
       }
+    } catch (error) {
+      response.json({ status: 141, error: error });
     }
-
-  } else {
-    response.json({ status: 141, error: "Invaild request signture" });
   }
 });
 
-// Start the server
+const port = 23000;
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
